be4e058288616f3d4d3c16de19017652ebdde859
Vlad Golubev <golubev.vld@gmail.com>
2015-09-30 20:27:44 +0300

    Add Android support

---
+++ mobile-config.js
@@ -0,0 +1,13 @@
+App.info({
+  name: 'MODULE.OK',
+  description: 'mod.tanet successor',
+  version: '1.0.0'
+});
+
+App.icons({
+  'android_ldpi': 'resources/icons/icon-ldpi.png',
+  'android_mdpi': 'resources/icons/icon-mdpi.png',
+  'android_hdpi': 'resources/icons/icon-hdpi.png',
+  'android_xhdpi': 'resources/icons/icon-xhdpi.png',
+  'android_xxhdpi': 'resources/icons/icon-xhdpi.png'
+});
