0a050992115bcf6eeab059494b4bb7534f90a78a
Vlad Golubev <golubev.vld@gmail.com>
2015-10-19 19:50:25 +0300

    Add splash screens

--- mobile-config.js
+++ mobile-config.js
@@ -9,28 +9,15 @@ App.icons({
   'android_hdpi': 'resources/icons/icon-hdpi.png',
   'android_xhdpi': 'resources/icons/icon-xhdpi.png'
 });
-//
-//App.launchScreens({
-//  // iOS
-//  'iphone': 'resources/splash/splash-320x480.png',
-//  'iphone_2x': 'resources/splash/splash-320x480@2x.png',
-//  'iphone5': 'resources/splash/splash-320x568@2x.png',
-//  'iphone6': 'resources/splash/splash-375x667@2x.png',
-//  'iphone6p_portrait': 'resources/splash/splash-414x736@3x.png',
-//  'iphone6p_landscape': 'resources/splash/splash-736x414@3x.png',
-//
-//  'ipad_portrait': 'resources/splash/splash-768x1024.png',
-//  'ipad_portrait_2x': 'resources/splash/splash-768x1024@2x.png',
-//  'ipad_landscape': 'resources/splash/splash-1024x768.png',
-//  'ipad_landscape_2x': 'resources/splash/splash-1024x768@2x.png',
-//
-//  // Android
-//  'android_ldpi_portrait': 'resources/splash/splash-200x320.png',
-//  'android_ldpi_landscape': 'resources/splash/splash-320x200.png',
-//  'android_mdpi_portrait': 'resources/splash/splash-320x480.png',
-//  'android_mdpi_landscape': 'resources/splash/splash-480x320.png',
-//  'android_hdpi_portrait': 'resources/splash/splash-480x800.png',
-//  'android_hdpi_landscape': 'resources/splash/splash-800x480.png',
-//  'android_xhdpi_portrait': 'resources/splash/splash-720x1280.png',
-//  'android_xhdpi_landscape': 'resources/splash/splash-1280x720.png'
-//});
+
+App.launchScreens({
+  // Android
+  'android_ldpi_portrait': 'resources/splash/splash-200x320.png',
+  'android_ldpi_landscape': 'resources/splash/splash-320x200.png',
+  'android_mdpi_portrait': 'resources/splash/splash-320x480.png',
+  'android_mdpi_landscape': 'resources/splash/splash-480x320.png',
+  'android_hdpi_portrait': 'resources/splash/splash-480x800.png',
+  'android_hdpi_landscape': 'resources/splash/splash-800x480.png',
+  'android_xhdpi_portrait': 'resources/splash/splash-720x1280.png',
+  'android_xhdpi_landscape': 'resources/splash/splash-1280x720.png'
+});
