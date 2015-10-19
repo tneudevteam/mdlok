4992ae892cc77e40d0aef9d41d99cf6c3a949bff
Vlad Golubev <golubev.vld@gmail.com>
2015-10-19 17:13:08 +0300

    Add mobile-config.js

--- mobile-config.js
+++ mobile-config.js
@@ -5,9 +5,7 @@ App.info({
 });
 
 App.icons({
-  'android_ldpi': 'resources/icons/icon-ldpi.png',
   'android_mdpi': 'resources/icons/icon-mdpi.png',
   'android_hdpi': 'resources/icons/icon-hdpi.png',
-  'android_xhdpi': 'resources/icons/icon-xhdpi.png',
-  'android_xxhdpi': 'resources/icons/icon-xhdpi.png'
+  'android_xhdpi': 'resources/icons/icon-xhdpi.png'
 });
